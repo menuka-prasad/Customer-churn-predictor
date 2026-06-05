9c19f1c74bb4c5c0db530c98b7d00938179b2587
menuka-prasad <menukaprasad0311@gmail.com>
2026-06-05 21:11:07 +0530

    fix build issues of frontend

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -1,7 +1,12 @@
 'use client';
 
+import { Suspense } from 'react';
 import { LoginPage } from '@/components/pages/auth/LoginPage';
 
 export default function Page() {
-  return <LoginPage />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginPage />
+    </Suspense>
+  );
 }
